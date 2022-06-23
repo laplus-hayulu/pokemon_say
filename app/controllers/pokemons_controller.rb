bf018a824bdfacc187e347dda0f8e56133c6ff57
kie <kie@manalabo.jp>
2022-06-23 05:10:52 +0000

    a

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,5 +2,12 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+	
+  def search
+   @pokemons = Pokemon.all
+   @pokemon_answer = []
+   @pokemons = Pokemon.search(params[:search])
+   @pokemon_answer.push(@pokemons)
+  end
 end
       
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,5 @@
 class Pokemon < ApplicationRecord
+  def self.search(search)
+      Pokemon.where(name: search.tr('ぁ-ん','ァ-ン'))
+  end
 end
